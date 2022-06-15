84e5f3ee3103c5b437aaa12f50c9e35511b548c6
jeremiahomueti <jeremiahomueti@gmail.com>
2022-06-15 13:04:33 +0100

    first commit

---
+++ multiple-returns.js
@@ -0,0 +1,12 @@
+function isFieldEmpty() {
+    const field = document.querySelector('#info');
+
+    if (!field.value) {return true;}
+    else {return false;}
+}
+
+const fieldTest = isFieldEmpty();
+
+if (fieldTest) {
+    alert('Please provide your information.')
+}
